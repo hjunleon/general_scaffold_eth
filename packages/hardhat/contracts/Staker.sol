0ec5e2858ecd42932f775fecbe95c37bd4de15dd
hjunleon <hjunleon@gmail.com>
2022-03-29 03:07:23 +0800

    Done challenge 1

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,31 +1,83 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+    constructor(address exampleExternalContractAddress) public {
+        exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    }
 
-  constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    mapping ( address => uint256 ) public balances;
+    uint256 public constant threshold = 1 ether;
+    uint256 public deadline = block.timestamp + 0.2 minutes;
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    /// Boolean set if threshold is not reached by the deadline
+    bool public openForWithdraw;
+    bool public hasExecute = false;
+
+    event Stake(address,uint256);
+
+    function timeLeft() view external returns(uint256){
+      if (block.timestamp >= deadline){
+        return 0;
+      }
+      return deadline - block.timestamp;
+    }
+
+    modifier _deadline_not_passed(){
+      require(this.timeLeft() > 0);
+      _;
+    }
+
+    function stake() public payable _deadline_not_passed{
+        balances[msg.sender] += msg.value;
+        emit Stake(msg.sender, msg.value);
+    }
+
+    function get_total_balance() view public returns(uint256){
+        return address(this).balance;
+    }
+
+    modifier _can_execute() {
+      require(!hasExecute);
+      require(block.timestamp >= deadline);
+      require(get_total_balance() >= threshold);
+      _;
+    }
 
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    function execute() external payable _can_execute{
+        hasExecute = true;
+        exampleExternalContract.complete{value: address(this).balance}();
+    }
+
+    modifier _can_withdraw(address payable _staker){
+        require(this.timeLeft() == 0 && get_total_balance() < threshold);
+        require(abi.encodePacked(balances[_staker]).length > 0);
+        _;
+    }
 
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
+    function withdraw(address payable _staker ) external _can_withdraw(_staker){
+        uint256 userBalance = balances[_staker];
+        balances[_staker]  = 0;
+        (bool sent, ) = _staker.call{value: userBalance}("");  //bytes memory data
+        require(sent, "Failed to send Ether");
+    } 
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    
 
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
-
+    // function receive() public payable {
+        
+    // }
+    receive() external payable {
+        stake();
+    }
 }
+
