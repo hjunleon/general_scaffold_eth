102b2f103e47c70358976d592a67cd187e0b7109
hjunleon <randell.crapy@gmail.com>
2022-04-15 01:53:21 +0800

    Test

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -26,7 +26,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "localhost";//"rinkeby";//"localhost";
 
 const mainnetGwei = 21;
 
@@ -73,7 +73,8 @@ module.exports = {
       */
     },
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://speedy-nodes-nyc.moralis.io/160246bc8af366c574f13842/eth/rinkeby",
+      // url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
